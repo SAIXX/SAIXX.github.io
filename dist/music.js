591b820f94c1b793e7959fef901e5589ee573b8d
SAIXX <652120532@qq.com>
2020-06-19 17:02:43 +0800

    Site updated: 2020-06-19 17:02:43

---
+++ dist/music.js
@@ -0,0 +1,22 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    autoplay: false,
+    loop: 'all',
+    volume: 0.7,
+    listFolded: true,
+    listMaxHeight: 60,
+    audio: [
+        {
+            name: 'name1',
+            artist: 'artist1',
+            url: 'url1.mp3',
+            cover: 'cover1.jpg',
+        },
+        {
+            name: 'name2',
+            artist: 'artist2',
+            url: 'url2.mp3',
+            cover: 'cover2.jpg',
+        }
+    ]
+});
