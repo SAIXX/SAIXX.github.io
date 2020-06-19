e7f083feaa2168623dc8f65b21df55ca7f993c27
SAIXX <652120532@qq.com>
2020-06-20 01:31:26 +0800

    Site updated: 2020-06-20 01:31:25

--- dist/music.js
+++ dist/music.js
@@ -1,9 +1,9 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     audio: [{
-        name: 'name',
-        artist: 'artist',
-        url: 'url.mp3',
+        name: 'molihua',
+        artist: 'minyao',
+        url: 'http://aod.cos.tx.xmcdn.com/group80/M0B/26/55/wKgPDF7VQmTjA4blACG2x6mE4uo759.mp3',
         cover: 'cover.jpg'
     }]
 });
