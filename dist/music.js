e0600013b317126ba85e50b7de2a824084cc2a1e
SAIXX <652120532@qq.com>
2020-06-20 00:38:46 +0800

    Site updated: 2020-06-20 00:38:46

--- dist/music.js
+++ dist/music.js
@@ -1,22 +1,9 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    autoplay: false,
-    loop: 'all',
-    volume: 0.7,
-    listFolded: true,
-    listMaxHeight: 60,
-    audio: [
-        {
-            name: 'name1',
-            artist: 'artist1',
-            url: 'url1.mp3',
-            cover: 'cover1.jpg',
-        },
-        {
-            name: 'name2',
-            artist: 'artist2',
-            url: 'url2.mp3',
-            cover: 'cover2.jpg',
-        }
-    ]
-});
+    audio: [{
+        name: 'name',
+        artist: 'artist',
+        url: '/musictest/约束.mp3',
+        cover: 'cover.jpg'
+    }]
+});;
