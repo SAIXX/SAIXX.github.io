dafe3766d4d97f7f9bef73e0baf04e9cd05ca15a
SAIXX <652120532@qq.com>
2020-06-20 00:24:43 +0800

    Site updated: 2020-06-20 00:24:43

--- dist/music.js
+++ dist/music.js
@@ -1,32 +1,25 @@
-const ap = new APlayer({
-    container: document.getElementById('player'),
-    mini: false,
+t ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
     autoplay: false,
-    theme: '#FADFA3',
-    loop: 'all',
-    order: 'random',
-    preload: 'auto',
-    volume: 0.7,
-    mutex: true,
-    listFolded: false,
-    listMaxHeight: 90,
-    lrcType: 3,
     audio: [
-        {
-            name: 'name1',
-            artist: 'artist1',
-            url: 'url1.mp3',
-            cover: 'cover1.jpg',
-            lrc: 'lrc1.lrc',
-            theme: '#ebd0c2'
-        },
-        {
-            name: 'name2',
-            artist: 'artist2',
-            url: 'url2.mp3',
-            cover: 'cover2.jpg',
-            lrc: 'lrc2.lrc',
-            theme: '#46718b'
-        }
+      {
+        name: "PDD洪荒之力",
+        artist: '徐梦圆',
+        url: 'http://up.mcyt.net/?down/39868.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
+      {
+        name: '9420',
+        artist: '麦小兜',
+        url: 'http://up.mcyt.net/?down/45967.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: '风筝误',
+        artist: '刘珂矣',
+        url: 'http://up.mcyt.net/?down/46644.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      }
     ]
 });
