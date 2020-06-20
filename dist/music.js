327136cb7fc6bb6760ebb8d3e9612bcbe0599dc4
SAIXX <652120532@qq.com>
2020-06-20 12:39:09 +0800

    Site updated: 2020-06-20 12:39:09

--- dist/music.js
+++ dist/music.js
@@ -9,7 +9,7 @@ const ap = new APlayer({
         {
             name: 'Hellraiser',
             artist: 'Christopher Young',
-            url: 'http://m10.music.126.net/20200620021528/d580c968e1912610cd11daee93df0421/ymusic/c64d/3641/f97d/83d06db3ffad33c96076001fd950b481.mp3',
+            url: 'http://m10.music.126.net/20200620130038/4bd6bf2528c97ddf7ff9b11f23ed5905/ymusic/c64d/3641/f97d/83d06db3ffad33c96076001fd950b481.mp3',
             cover: 'cover1.jpg',
         }
     ]
