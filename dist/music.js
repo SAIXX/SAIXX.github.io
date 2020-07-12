6823a94a9f9af3e755dcc418e054253fd197015b
SAIXX <652120532@qq.com>
2020-07-12 14:57:05 +0800

    Site updated: 2020-07-12 14:57:04

--- dist/music.js
+++ dist/music.js
@@ -7,9 +7,9 @@ const ap = new APlayer({
     listMaxHeight: 60,
     audio: [
         {
-            name: 'Hellraiser',
-            artist: 'Christopher Young',
-            url: 'http://m10.music.126.net/20200620130038/4bd6bf2528c97ddf7ff9b11f23ed5905/ymusic/c64d/3641/f97d/83d06db3ffad33c96076001fd950b481.mp3',
+            name: 'no future',
+            artist: 'no future',
+            url: 'http://www.saixx.cn/2020/07/09/1.mp3',
             cover: 'cover1.jpg',
         }
     ]
